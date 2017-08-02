60f8567b152ceb5e2c2211c8e4272979a2ed5ed3
Unstable <unstablectrl@gmail.com>
2017-08-02 01:39:54 +0100

    Add !gif command
    Search for gifs using the bot command !gif <tag>. Powered by GIPHY

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+from urllib.request import urlopen
+from urllib.parse  import urlencode
+import json
 
 TOKEN = 'MzM5NTUwOTc5Mzk1NDIwMTcx.DF_rkQ.GVPIU-ouYQHUToX_lKVzYuV4AjI'
 
@@ -24,5 +27,19 @@ async def hello():
 async def add(left : int, right : int):
     """Adds two numbers together."""
     await bot.say(left + right)
+
+
+@bot.command()
+async def gif(tag=''):
+    """Search for gif"""
+    query = 'https://api.giphy.com/v1/gifs/random?'
+    params = urlencode({
+        'api_key': '62b0f8d737c7419ba2620fcfb6b02a12',
+        'tag': tag,
+        'rating': 'G' # [Y, G, PG, PG-13, R]
+        })
+    res = json.loads(urlopen(query+params).read())
+    img = 'https://i.giphy.com/' + res['data']['id'] + '.gif'
+    await bot.say(img)
 
 bot.run(TOKEN)
